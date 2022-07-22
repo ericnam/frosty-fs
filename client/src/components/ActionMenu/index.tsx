b6a9c845b37e2d79ddddb39b252611ae0be9628e
ericnam <ericnam86@utexas.edu>
2022-07-22 18:06:06 -0500

    Action ddl update

--- client/src/components/ActionMenu/index.tsx
+++ client/src/components/ActionMenu/index.tsx
@@ -3,16 +3,24 @@ import {
   ActionMenuStore,
   IActionMenuStore,
 } from "contexts/actionMenu.provider";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFolder,
+  faStar,
+  faTrashCan,
+} from "@fortawesome/free-regular-svg-icons";
 
 const ActionMenu = () => {
-  const [actionIconWidth] = useState<number>(300);
+  const [actionIconWidth] = useState<number>(250);
   const { actionMenuState } = useContext<IActionMenuStore>(ActionMenuStore);
 
   return (
     <div
       className={`${
         actionMenuState.isActive ? "" : "hidden"
-      } absolute bg-gray-50 p-4 z-10`}
+      } absolute border bg-white shadow-lg z-10`}
       style={{
         left: actionMenuState.posX + 10,
         top: actionMenuState.posY,
@@ -20,8 +28,28 @@ const ActionMenu = () => {
         height: "auto",
       }}
     >
-      asdf
+      <ul className={`my-2`}>
+        <ActionMenuLineItem text="Rename" icon={faPencil as IconProp} />
+        <ActionMenuLineItem text="Move to" icon={faFolder as IconProp} />
+        <ActionMenuLineItem text="Add to favorites" icon={faStar as IconProp} />
+        <ActionMenuLineItem text="Delete" icon={faTrashCan as IconProp} />
+      </ul>
     </div>
+  );
+};
+
+interface IActionMenuLineItemProps {
+  text: string;
+  icon: IconProp;
+}
+const ActionMenuLineItem = ({ text, icon }: IActionMenuLineItemProps) => {
+  return (
+    <li
+      className={`text-sm px-6 py-2 hover:bg-gray-100 flex items-center text-gray-900 cursor-pointer`}
+    >
+      <FontAwesomeIcon icon={icon} className={`mr-4`} />
+      <span>{text}</span>
+    </li>
   );
 };
 
