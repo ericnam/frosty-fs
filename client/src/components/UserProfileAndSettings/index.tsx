abbc5d2c35df6f1d4089440670b2619b83495628
ericnam <ericnam86@utexas.edu>
2022-07-23 10:19:24 -0500

    initial profile and settings checkin

---
+++ client/src/components/UserProfileAndSettings/index.tsx
@@ -0,0 +1,22 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faGear, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const UserProfileAndSettings = (): JSX.Element => {
+  return (
+    <div className={`flex flex-row`}>
+      <span
+        className={`text-gray-300 flex items-center justify-center h-10 w-10 cursor-pointer hover:text-gray-500`}
+      >
+        <FontAwesomeIcon icon={faGear as IconProp} />
+      </span>
+      <span
+        className={`text-gray-500 h-10 w-10 flex items-center justify-center rounded-full ml-4 bg-gray-100 cursor-pointer`}
+      >
+        <FontAwesomeIcon icon={faUser as IconProp} />
+      </span>
+    </div>
+  );
+};
+
+export default UserProfileAndSettings;
